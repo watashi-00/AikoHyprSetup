88f6e85c32df5c3f49c21649d80e3fc059d63138
watashi-00 <kalitajj374@gmail.com>
2026-06-04 19:25:02 -0300

    chore: bump version to 2.1.7

--- scripts/lib/utils.sh
+++ scripts/lib/utils.sh
@@ -4,7 +4,7 @@
 # Sourced by main scripts for consistent behavior and reduced redundancy.
 
 # --- Version ---
-export AIKO_VERSION="2.1.6"
+export AIKO_VERSION="2.1.7"
 
 # --- Colors ---
 export NC=$'\e[0m'
