ae90a8de66f9b0b6ee345432413d6a641ecdee1d
watashi-00 <kalitajj374@gmail.com>
2026-06-04 18:51:11 -0300

    chore: bump version to 2.1.5 to synchronize new update system

--- scripts/lib/utils.sh
+++ scripts/lib/utils.sh
@@ -4,7 +4,7 @@
 # Sourced by main scripts for consistent behavior and reduced redundancy.
 
 # --- Version ---
-export AIKO_VERSION="2.1.4"
+export AIKO_VERSION="2.1.5"
 
 # --- Colors ---
 export NC=$'\e[0m'
